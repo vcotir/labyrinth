402cccdcb25a996b0ebaf8c981cee3ae882a35db
Victor Ngo <psychosete@gmail.com>
2017-12-22 17:14:21 -0800

    (feat) Implement fail-safe to recheck failed HTTP requests (higher successful report rate)

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 > cd labyrinth_victor_ngo;
 > npm i;
 
-## Execution (assumes you're within repo directory)
+## Running (assumes you're within repo directory)
 > node index.js
 
 Purpose: The purpose of the program is to interact correctly with the cyberspace labryinth to generate and send the correct report. 
@@ -14,6 +14,78 @@ Reminder to self:
 1) Write clean, easy to follow, well commented code
 2) Demonstrate quality
 
-Design Decisions:
-1) 
+#TODO List
+[x] THINK about how to solve problem!
+[x] Formulate plan of attack (make diagram!)
+[] Complete design decisions section
+
+#High Level Plan
+1) Create helper functions for separating concerns and easier development
+
+ULTIMATE GOAL: 
+- Send report to /report in desired format w/ required information
+
+# Thought process
+
+1) What is the required information?
+  A) roomIds: [Array of roomId's with lights broken]
+  B) challenge: "<challenge code>" (from concatenating writing on wall of lit room in order)
+
+2) How do we get 1.A?
+  A) Need to check all roomIds and store ones with light off
+    - get all roomIds with an order of -1 in response object's 'roomIds' array
+
+3) How do we get 1.B?
+  A) Need to check all roomIds and store ones with lights on
+    - sort all roomIds by 'order' property
+    - concatenate the sorted roomId's writings into string
+    - assign above string to 'challenge' property
+
+4) Can we combine Step 2 and 3?
+  A) Get all roomIds and check the writing on the wall of all of them
+    - store writing on the wall for sorting later
+      - Resultant shape: [{'exampleRoomId': {writing: 'F', order: 4}}, ...]
+  B) Iterate thru all roomIds
+    - Store roomIds with -1 (lights off) order on writing on the wall (hereon called "WOTW") in roomIds array
+    - Place all roomIds with lights on into array (for sorting!)
+    - Sort above array by 'order' property lowest to greatest
+    - Iterate thru above array
+      - Concatate string to form challenge code
+      - assign challenge code to 'challenge' property of response object
+
+5) How can we get ALL roomIds?
+  A) Get start roomId
+  B) Check exits of start roomId
+  C) For each element in exit
+  D) Find roomIds of each room
+  E) Store roomId in roomIds array
+
+6) Seems like we have some work in Step 5 that can be done recursively?
+  Plan of attack (w/ recursion in mind)
+  A) Get start roomId
+  B) Push into roomId into roomIds
+  C) Find exits of this roomId
+  D) For each direction in exits
+  E) Peek to get roomId of room in that direction
+  F) Repeat steps B) --> E) until explored all rooms
+
+  Recursive function
+  Input: roomId, Output: none, Side Effect
+  A) Push into roomId into roomIds
+  B) Find exits of this roomId
+  C) For each direction in exits
+  D) Peek to get roomId of room in that direction
+  E) Recursive case: Invoke recursive function on those roomIds
+  F) Base case: When there are no children (enforced by the loop. Recursive won't happen when no children)
+
+7) Dealing with asynchronicity of recursive function with multiple elements
+  A) Utilize Promise.all
+
+#Design Decisions:
+1) Why use axios?
+- B/c of its promise based interface + HTTP client
+
+2) Why make so many helper functions?
+- Abstraction to make it easier to
+
 
--- index.js
+++ index.js
@@ -1,1 +1,262 @@
+const axios = require('axios');
+const _ = require('underscore');
+
+const config = {
+  headers: {
+    'X-Labyrinth-Email': 'victorkjngo@gmail.com'
+  }
+};
+
+let roomIds = [];
+
+const renderConfigWithParams = (config, params) => {
+  // purpuse: this function extends the original configureation object to have addition params (query string key value pairs)
   
+  // input: object (original config object), object (key value pairs to be attached )
+  var config = _.extend({
+    params: params
+  }, config);
+
+  return config;
+};
+
+const request = (endpoint, params, data, waitTime = 0) => {
+  // Purpose: this function is invoked  
+
+  // input: string (endpoint name), object (parameters), object (request body, for post requests)
+  // output: promisified result
+  console.log('INVOKING REQUEST WITH THIS', endpoint, 'params', params, 'data', data, 'after', waitTime, 'ms');  
+  const url = 'http://challenge2.airtime.com:7182/' + endpoint;
+  let method = '';
+  let callback = ({data}) => {
+    return data;
+  };
+
+  const {roomId} = params;
+  let newConfig = _.extend({}, config);
+
+  switch (endpoint) {
+    case 'start': 
+      method = 'get';
+      break;
+    case 'exits':
+      method = 'get';
+      newConfig = renderConfigWithParams(config, {
+        roomId
+      });
+
+      callback = ({data}) => {
+        const {exits} = data;
+        return exits;
+      };
+
+      break;
+    case 'move':
+      method = 'get';
+      let {exit} = params;
+      newConfig = renderConfigWithParams(config, {
+        exit,
+        roomId
+      });
+
+      callback = ({data}) => {
+        const {roomId} = data;
+        return roomId;
+      };
+      break;
+    case 'wall':
+      method = 'get';
+      newConfig = renderConfigWithParams(config, {
+        roomId
+      });
+      break;
+    case 'report':
+      method = 'post';
+      break;
+
+    default:
+  }
+
+  switch (method) {
+    case 'GET':
+    case 'get':
+      return axios.get(url, newConfig)
+        .then(callback)
+        .catch(err => {
+          console.log('Invoking request again with', endpoint, 'params', params, 'data', data, 'after', waitTime, 'ms');
+          return new Promise ((resolve, reject) => {
+            setTimeout(function () {
+              request(endpoint, params, data, waitTime)
+                .then(output => {
+                  console.log('SUCCESS. Second times the charm!');
+                  resolve(output);
+                });
+            }, waitTime += 500);
+          });
+        });
+      break
+    case 'POST', 'post':
+      // console.log('DATA', data); 
+      console.log('REPORT PROPERLY GENERATED. \nReady to send:', data);
+      return axios.post(url, data, newConfig)
+      .then(function (res) {
+        console.log('Response from server', res.data);
+        return res.data;  
+      }); 
+      break;
+  }
+  
+};
+
+const findStartId = () => {
+  // purpose: returns the start roomId
+
+  // input: n/a
+  // o: promisified roomId
+
+  return request('start', {});
+};
+
+const findExitDirections = (roomId) => {
+  // purpose: return the exits of a given roomId
+
+  // input: string (roomId)
+  // o: promisified array of exits
+
+  return request('exits', {roomId});
+};
+
+const peekExitId = (exit, roomId) => {
+  // purpose: return roomId of the room through a certain exit
+
+  // input: n/a
+  // o: promisified roomId
+  
+  return request('move', {roomId, exit});
+};
+
+const asyncMap = (array, callback, roomId) => {
+  // purpose: return a promisified array with promisified values
+
+  // i: array, function (to be run on values of the array), string (optional roomId)
+  // o: promified array
+
+  if (!array || !array.length) {
+    return [];
+  }
+
+  const promises = [];
+  
+  array.forEach(el => {
+    promises.push(callback(el, roomId));
+  });
+
+  return Promise.all(promises);
+};
+
+const findRoomExitIds = (roomId) => {
+  // purpose: given roomId, return list of exit roomIds
+
+  // I: string (roomId)
+  roomIds.push(roomId);
+  return findExitDirections(roomId)
+    .then(exits => {
+      return asyncMap(exits, peekExitId, roomId)
+    })
+    .then(exitIds => {
+      return asyncMap(exitIds, findRoomExitIds);
+    });
+};
+
+const checkWriting = (roomId) => {
+  // purpose: return the writing on wall of a room
+
+  // input: string (roomId)
+  // o: promisified object (writing on the wall)
+
+  return request('wall', {roomId});
+};
+
+const postReport = (report) => {
+  // Posts report to be 
+  return request('report', {}, report);
+};
+
+const sortAscendingByOrder = (a, b) => {
+  return a.order - b.order;
+};
+
+function buildAndPostReport () {
+  roomIds = [];
+
+  return findStartId()
+    .then(({roomId}) => { 
+      roomIds.push(roomId);
+      return Promise.all([findExitDirections(roomId), roomId]);
+    })
+    .then(([exits, roomId]) => {
+      return asyncMap(exits, peekExitId, roomId);
+    })
+    .then(exitIds => {
+      return asyncMap(exitIds, findRoomExitIds);
+    })
+    .then(() => {
+      const promises = [];
+      roomIds.forEach(roomId => {
+        promises.push(Promise.all([roomId, checkWriting(roomId)]));
+      });
+
+      return Promise.all(promises);
+    })
+    .then(roomIdsWritings => {
+      const post = {
+        roomIds: [],
+        challenge: ''
+      };
+
+      const litupRoomsWritings = [];
+
+      roomIdsWritings.forEach(([roomId, writing]) => {
+        if (writing.order === -1) {
+          post.roomIds.push(roomId);
+        } else {
+          litupRoomsWritings.push(writing);
+        }
+      });
+
+      litupRoomsWritings.sort(sortAscendingByOrder);
+
+      litupRoomsWritings.forEach(({writing}) => {
+        post.challenge += writing;
+      });
+
+      return postReport(post);
+    })
+    .then(async (res) => {
+      console.log('RESPONSE', res);
+      let isSuccess = res === 'Congratulations!  Your work is complete.  Please send your code to challenge@airtime.com';
+      if(!isSuccess) {
+        console.log('BAD REPORT, TRYING AGAIN');
+        return await buildAndPostReport();
+      } 
+      return res;
+    })
+    .catch(err => {
+      console.error('ERROR', err);
+      // buildAndPostReport();
+    });
+
+}
+
+// async function recurse () { 
+//   var count = 0;
+//   while (true) {
+//     count++;
+//     console.log('ATTEMPT #', count);
+//     await buildAndPostReport();  
+//   }
+  
+// }
+
+buildAndPostReport();
+// recurse();
